be5019877d9ee9ae9a4731b3096e7affab17fa8d
llirik111 <149056208+llirik111@users.noreply.github.com>
2025-10-02 23:27:23 +1000

    Add files via upload

---
+++ listik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace project {
+    
+    class Program
+    {
+        static void Main()
+        {
+            int number = -10;
+            Console.WriteLine(number);
+            Console.ReadLine();
+            
+        }
+
+    }
+
+}
